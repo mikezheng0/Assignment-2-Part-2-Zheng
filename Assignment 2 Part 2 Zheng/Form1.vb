6adfd589ba5588b7c77dc3ec721695fe2c7bfc1d
mikezheng0 <mike_zheng0@hotmail.com>
2015-10-17 08:36:09 -0400

    Added Services

--- Assignment 2 Part 2 Zheng/Form1.vb
+++ Assignment 2 Part 2 Zheng/Form1.vb
@@ -5,6 +5,7 @@
     Const decRCHAMBERS As Decimal = 40D
     Const decSPALLON As Decimal = 50D
     Const decLRENKINS As Decimal = 55D
+    Dim decServiceArray As Decimal() = {30D, 40D, 50D, 200D, 60D, 50D}
 
     Dim decBaseCost As Decimal = 0
     Private Sub cboHairdressers_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboHairdressers.SelectedIndexChanged
@@ -22,6 +23,14 @@
     End Sub
 
     Private Sub lstServices_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstServices.SelectedIndexChanged
+        If (cboHairdressers.SelectedIndex >= 0) Then
+            cboHairdressers.Enabled = False
+            btnAddService.Enabled = True
+            lstDisplayedPrice.Items.Add(lstServices.SelectedItem + " " + decServiceArray(lstServices.SelectedIndex).ToString())
+        End If
+    End Sub
 
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
     End Sub
 End Class
